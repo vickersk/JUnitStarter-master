0d621808d02aba62d962e8d4a58252f4fcb810d8
vickersk <kaiv14@comcast.net>
2020-03-23 20:04:05 -0700

    Checkpoint 2

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -1,18 +1,37 @@
 package up.edu.dimcalc;
 
+import android.graphics.Point;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class TwoPointsTest {
 
+    /** when created, getPoint() should show both points at the origin */
     @Test
-    public void getPoint() {
+    public void getPoint() throws Exception {
+        TwoPoints testPoints = new TwoPoints();
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(1, p1.x);
+        assertEquals(0, p1.y);
+        assertEquals(0, p2.x);
+        assertEquals(0, p2.y);
     }
 
+    /** verify that arbitrary values are properly stored via setPoint() */
     @Test
-    public void setPoint() {
+    public void setPoint() throws Exception {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(5, p1.x);
+        assertEquals(-3, p1.y);
+        assertEquals(-3, p2.x);
+        assertEquals(5, p2.y);
     }
+
 
     @Test
     public void randomValue() {
