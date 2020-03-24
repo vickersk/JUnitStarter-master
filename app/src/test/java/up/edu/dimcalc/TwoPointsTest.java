f8708c0d694ee39250769f132613a6e8b94e6ef3
vickersk <kaiv14@comcast.net>
2020-03-23 19:58:00 -0700

    Checkpoint 1

---
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -0,0 +1,36 @@
+package up.edu.dimcalc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwoPointsTest {
+
+    @Test
+    public void getPoint() {
+    }
+
+    @Test
+    public void setPoint() {
+    }
+
+    @Test
+    public void randomValue() {
+    }
+
+    @Test
+    public void setOrigin() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void distance() {
+    }
+
+    @Test
+    public void slope() {
+    }
+}
